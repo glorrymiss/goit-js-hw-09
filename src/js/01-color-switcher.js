a9567530b5d4ba39cbada10eb1357d7b14711308
glorrymiss <113041245+glorrymiss@users.noreply.github.com>
2023-02-03 11:44:27 +0100

    add src and result ex1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,24 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+
+btnStart.addEventListener('click', hendleClickOnBtnStart);
+btnStop.addEventListener('click', hendleClickOnBtnStop);
+
+function hendleClickOnBtnStart(event) {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+}
+
+function hendleClickOnBtnStop(event) {
+  btnStop.disabled = true;
+  btnStart.disabled = false;
+
+  clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
