28e2b701cd14897235692b2ec0fc1353824d0e9f
glorrymiss <113041245+glorrymiss@users.noreply.github.com>
2023-02-06 14:14:43 +0000

    Deploying to gh-pages from @ glorrymiss/goit-js-hw-09@7ba8ec9f2f9a216df5a2a2a69b4b5d93a8bf9695 🚀

--- 01-color-switcher.68834515.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(function(d){timerId=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(d){e.disabled=!0,t.disabled=!1,clearInterval(timerId)}));
-//# sourceMappingURL=01-color-switcher.68834515.js.map
--- 01-color-switcher.f6b53981.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(function(n){timerId=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(n){e.disabled=!0,t.disabled=!1,clearInterval(timerId)}))}();
-//# sourceMappingURL=01-color-switcher.f6b53981.js.map
