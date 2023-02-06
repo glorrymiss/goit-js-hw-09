65a3b4bab764c225b03a6c05263e92e894bd2e69
glorrymiss <113041245+glorrymiss@users.noreply.github.com>
2023-02-06 11:55:40 +0100

    add ex 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,81 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+import 'notiflix/build/notiflix-notify-aio';
+
+// КРОКИ
+
+// 1.ВИТЯГНУТИ ЗНАЧЕННЯ ІНПУТІВ ДОСТУКУЄМОСЬ ДО ФОРМИ
+// 2. НА ФОРМУ СТВОРИТИ ОПЕРАТОРА З ПОДІЄЮ САБМІТ ЯКИЙ МАЄ ФУНКЦІЮ СТВОРИТИ ПРОМІСИ(СМС)
+// 3.В  ПРОМІСІ ПРОПИСАТИ AMOUNT - К-СТЬ ПРОМІСІВ ЯКІ МАЮТЬ СТВОРИТИСЬ
+// 4. В ДЕЛАЙ - ЗАПИСУЄМО ЧАС ІЗ ЗАТРИМКОЮ СТВОРЕННЯ ПРОМІСІВ
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', hendleClickSubmit);
+// form.addEventListener('input', hendleInputDate);
+function hendleClickSubmit(event) {
+  event.preventDefault();
+  //  достукалась до значення інпутів форми
+  const {
+    elements: { delay, step, amount },
+  } = event.currentTarget;
+  console.log(delay.value);
+  //   проходимо цикл додаємо на кожному проміс час кроку
+  let timer = +delay.value;
+  for (let i = 1; i <= amount.value; i++) {
+    if (i === 1) {
+      timer = +delay.value;
+      createPromise(i, timer);
+      continue;
+    } else {
+      timer += +step.value;
+    }
+    createPromise(i, timer);
   }
+  console.log(timer);
 }
+// функція створення промісів рандомно
+function createPromise(position, delay) {
+  setTimeout(() => {
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      // Fulfill
+
+      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    } else {
+      // Reject
+
+      Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    }
+  }, delay);
+}
+
+// createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     Notiflix.Notify.success(resalt);
+//   })
+//   .catch(({ position, delay }) => {
+//     Notiflix.Notify.failure(error);
+//   });
+
+// const promise = new Promise((resolve, reject) => {
+//   const shouldResolve = Math.random() > 0.3;
+//   // setTimeOut(() => {
+//   if (shouldResolve) {
+//     // Fulfill
+//     resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   }
+//   // Reject
+//   reject(`❌ Rejected promise ${position} in ${delay}ms`);
+//   // }, delay);
+// });
+// return promise;
+// }
+
+// createPromise(amount, firstDelay);
+
+//   .then(({ position, delay }) => {
+//     Notiflix.Notify.success(result);
+//   })
+//   .catch(({ position, delay }) => {
+//     Notiflix.Notify.warning(error);
+//   });
